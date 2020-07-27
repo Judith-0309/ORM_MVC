01b6e4c60031aa73ae6b5edd2ad84037a29c7033
Judith-0309 <sagnajudith@gmail.com>
2020-07-27 15:58:39 +0000

    first commit

---
+++ Src/entities/Clientparticulier.php
@@ -0,0 +1,164 @@
+<?php
+//les espaces de noms l'annotation qui est une classe dans Doctrine sera mapper à ORM
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity @ORM\Table(name="clientparticulier")
+ */
+
+class Clientparticulier
+{
+    //Pour les clés primaires
+    /** @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue **/
+     
+    private $id;
+    /** @ORM\Column(type="string") **/ 
+    private $nom;
+    /** @ORM\Column(type="string") **/ 
+    private $prenom;
+    /** @ORM\Column(type="string") **/ 
+    private $telephone;
+     /** @ORM\Column(type="string") **/
+    private $genre;
+     /** @ORM\Column(type="text") **/
+    private $email;
+    /** @ORM\Column(type="string") **/ 
+    private $adresse;
+     /** @ORM\Column(type="string") **/ 
+    private $profession;
+     /** @ORM\Column(type="string") **/ 
+    private $salarie;
+     /** @ORM\Column(type="text") **/ 
+    private $salarie_actuel;
+     /** @ORM\Column(type="decimal") **/ 
+    private $nom_employeur;
+     /** @ORM\Column(type="text") **/ 
+    private $cni;
+    private $numeroCompte;
+    /** @ORM\Column(type="string") **/ 
+     /**
+     * One Clientparticulier as many compte. This is the inverse side.
+     * @OneToMany(targetEntity="compte", mappedBy="Clientparticulier")
+     */
+
+
+
+public function __construct()
+{
+
+}
+    public function getId()
+    {
+       return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id=$id;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function setNom($nom)
+    {
+        $this->nom=$nom;
+    }
+
+        public function getPrenom()
+    {
+            return $this->prenom;
+    }
+    public function setPrenom($prenom)
+    {
+           $this->prenom=$prenom;
+    }
+
+        public function getTelephone()
+    {
+            return $this->telephone;
+    }
+    public function setTelephone($telephone)
+    {
+            $this->telephone=$telephone;
+    }
+
+
+        public function getGenre()
+    {
+            return $this->genre;
+    }
+    public function setGenre($genre)
+    {
+            $this->genre=$genre;
+    }
+
+        public function getEmail()
+    {
+            return $this->email;
+    }
+    public function setEmail($email)
+    {
+             $this->email=$email;
+    }
+
+        public function getAdresse()
+    {
+            return $this->adresse;
+    }
+    public function setAdresse($adresse)
+    {
+           $this->adresse=$adresse;
+    }
+
+        public function getProfession()
+    {
+            return $this->profession;
+    }
+    public function setProfession($profession)
+    {
+           $this->profession=$profession;
+    }
+
+        public function getSalarie()
+    {
+            return $this->salarie;
+    }
+    public function setSalarie($salarie)
+    {
+             $this->salarie=$salarie;
+    }
+
+
+        public function getSalaireActuel()
+    {
+            return $this->salarie_actuel;
+    }
+    public function setSalaireActuel($salarie_actuel)
+    {
+            $this->salarie_actuel=$salarie_actuel;
+    }
+
+        public function getNomEmployeur()
+    {
+            return $this->nomEmployeur;
+    }
+    public function setNomEmployeur($nom_employeur)
+    {
+           $this->nomEmployeur=$nom_employeur;
+    }
+
+        public function getCni()
+    {
+            return $this->cni;
+    }
+    public function setCni($cni)
+    {
+           $this->cni=$cni;
+    }
+}
+
+
+
+
+
---
+++ Src/entities/Compte.php
@@ -0,0 +1,125 @@
+<?php
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity @ORM\Table(name="compte")
+ */
+class Compte
+{
+    /** @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue **/
+
+    private $id;
+     /** @ORM\Column(type="string") **/
+    private $typeCompte;
+     /** @ORM\Column(type="string") **/
+    private $idEntreprise;
+      /**
+        * @ORM\ManyToOne(targetEntity="Clientparticulier",inversedBy="compte")
+      */
+    private $idClientPhysique;
+     /** @ORM\Column(type="string") **/
+    private $NumeroCompte;
+     /** @ORM\Column(type="string") **/
+    private $CleRib;
+     /** @ORM\Column(type="string") **/
+    private $EtatCompte;
+     /** @ORM\Column(type="string") **/
+    private $DepotInitial;
+     /** @ORM\Column(type="string") **/
+    private $DateOuverture;
+     /** @ORM\Column(type="integer") **/
+
+   
+
+    public function __construct()
+    {
+
+    }
+    //getters setters
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id=$id;
+    }
+
+    public function getTypeCompte()
+    {
+        return $this->typeCompte;
+    }
+    public function setTypeCompte($typeCompte)
+    {
+        $this->typeCompte=$typeCompte;
+    }
+
+    public function getIdEntreprise()
+    {
+        return $this->idEntreprise;
+    }
+    public function setIdEntreprise($idEntreprise)
+    {
+        $this->idEntreprise=$idEntreprise;
+    }
+
+    public function getIdClientPhysique()
+    {
+        return $this->idClientPhysique;
+    }
+    public function setIdClientPhysique($idClientPhysique)
+    {
+        $this->idClientPhysique=$idClientPhysique;
+    }
+
+        public function getNumeroCompte()
+    {
+            return $this->NumeroCompte;
+    }
+    public function setNumeroCompte($NumeroCompte)
+    {
+           $this->NumeroCompte=$NumeroCompte;
+    }
+
+        public function getCleRib()
+    {
+            return $this->CleRib;
+    }
+    public function setCleRib($CleRib)
+    {
+           $this->CleRib=$CleRib;
+    }
+
+        public function getEtatCompte()
+    {
+            return $this->EtatCompte;
+    }
+    public function setEtatCompte($EtatCompte)
+    {
+            $this->EtatCompte=$EtatCompte;
+    }
+
+        public function getDepotInitial()
+    {
+            return $this->DepotInitial;
+    }
+    public function setDepotInitial($DepotInitial)
+    {
+           $this->DepotInitial=$DepotInitial;
+    }
+
+        public function getDateOuverture()
+    {
+            return $this->DateOuverture;
+    }
+    public function setDateOuverture($DateOuverture)
+    {
+             $this->DateOuverture=$DateOuverture;
+    }
+       
+}
+    
+
+
+
+
+
